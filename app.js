62c5bfe79b0430896ed06754312564d3693a63c8
Rabia105578 <132687493+Rabia105578@users.noreply.github.com>
2023-05-05 20:20:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Questions1
+var myName="Rabia"
+alert(myName);
+// Questions2
+var titleMsg="Hello everybody"
+alert(titleMsg);
+// Question3
+var myName="Rabia"
+var myAge="16"
+var myQualification="Intermidiate"
+
+alert(myName);
+alert(myAge);
+alert(myQualification);
+// Question4
+alert("Pizza\nPizz\nPiz\nPi\nP");
+// Question5
+var myEmail="My Email adress is rabiakhan486@gmail.com"
+alert(myEmail);
+// Question6
+var myBook="Iam trying to learnfrom the book A smarter way to learn Javascript "
+alert(myBook);
+//Question7
+var pattern="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(pattern);
