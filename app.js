aca0128bd1a4caad214788299fe074511355a27f
Rabia105578 <132687493+Rabia105578@users.noreply.github.com>
2023-05-13 21:00:22 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-// Questions1
-var myName="Rabia"
-alert(myName);
-// Questions2
-var titleMsg="Hello everybody"
-alert(titleMsg);
-// Question3
-var myName="Rabia"
-var myAge="16"
-var myQualification="Intermidiate"
+// // // =====Q1=======//
 
-alert(myName);
-alert(myAge);
-alert(myQualification);
-// Question4
-alert("Pizza\nPizz\nPiz\nPi\nP");
-// Question5
-var myEmail="My Email adress is rabiakhan486@gmail.com"
-alert(myEmail);
-// Question6
-var myBook="Iam trying to learnfrom the book A smarter way to learn Javascript "
-alert(myBook);
-//Question7
-var pattern="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
-alert(pattern);
+// // //  let age=16;
+// // //  alert("I am"+age+"years old")
+// // //  ====Q2=====\\
+// //  let number_of_visits=0;
+// //  let i=0;
+// //  for (i=0;i<1000;i++){
+// //      number_of_visits=number_of_visits+1;
+// //      alert("you have visited this site"+number_of_visits+"times")
+// //  }
+// // function visits( number_of_visits){
+// //     return number_of_visits;
+// // }
+// // // =======Q3=======\\
+
+// // let birth_year=2007;
+// // document.write("my birth year is"+birth_year);
+// // document.write("<br>");
+// // document.write("data type of my declered variable of number");
+// // =====Q4======//
+// let client_name = "rabiakhan";
+// let no_of_orders = 10;
+// let product_title = "pent";
+//  document.write( "<b>" + client_name  + "</b>"+ " ordered " + "<b>"+ no_of_orders  + " "+ product_title +  "</b>(s)"+ " on xyz clothing store");
